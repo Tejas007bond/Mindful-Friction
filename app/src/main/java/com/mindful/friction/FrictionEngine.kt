2e83d7a2f9d12c534257ea54d5efd6724223b925
Tejas007bond <tejasphalke762@gmail.com>
2026-06-18 20:53:06 +0530

    Imported the libraries in FrictionEngine.kt

---
+++ app/src/main/java/com/mindful/friction/FrictionEngine.kt
@@ -0,0 +1,7 @@
+package com.mindful.friction
+
+import android.content.Context
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.os.VibratorManager
+import android.util.Log
