f251b83b7ff025a17a5fbc1e89ce36aded0bf6ba
Tejas007bond <tejasphalke762@gmail.com>
2026-06-18 21:08:07 +0530

    Created a vibrator initialization method

--- app/src/main/java/com/mindful/friction/FrictionEngine.kt
+++ app/src/main/java/com/mindful/friction/FrictionEngine.kt
@@ -5,3 +5,14 @@ import android.os.VibrationEffect
 import android.os.Vibrator
 import android.os.VibratorManager
 import android.util.Log
+
+class FrictionEngine(private val context: Context, private val onTriggerUI: () -> Unit) {
+    private val vibrator: Vibrator by lazy {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S) {
+            (context.getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager).defaultVibrator
+        } else {
+            @Suppress("DEPRECATION")
+            context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        }
+    }
+}
